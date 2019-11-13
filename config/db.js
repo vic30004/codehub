ee418d79f45bcb6be9649e0ea8671049e3b900c2
vic30004 <victor_993@msn.com>
2019-11-12 19:04:00 -0800

    connected to db

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    });
+    console.log('MongoDB connected'.cyan.underline)
+  } catch (err) {
+      console.log(err.message);
+      process.exit(1);//exit process with failur
+  }
+};
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
-
+const connectDB = require('./config/db')
+const colors = require('colors')
 
 const app = express();
 
+// connect db 
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
-    console.log(`Server listening on Port ${PORT}`)
+    console.log(`Server listening on Port ${PORT}`.yellow.bold)
 })
 
