8908d45981a3f0a6bafe6510a5041920daf6067f
vic30004 <victor_993@msn.com>
2019-11-12 19:32:03 -0800

    basic route setup

---
+++ middleware/error.js
@@ -0,0 +1,9 @@
+const errorHandler = (err,req,res,next)=>{
+    // Log to console for dev 
+    console.log(err.stack.red);
+
+    res.status(500).json({
+        success:false,
+        error: err.message
+    });
+}
--- server.js
+++ server.js
@@ -1,15 +1,38 @@
-const express = require('express');
-const connectDB = require('./config/db')
-const colors = require('colors')
+const express = require("express");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
 
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// initialize express
 const app = express();
 
-// connect db 
+// connect db
 connectDB();
+
+// Log routes 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.get("/", (req, res) => res.send("API Running"));
+// Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
+// Dev logging middleware
+
+// dont put this before routes.
+// app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`Server listening on Port ${PORT}`.yellow.bold)
-})
-
+app.listen(PORT, () => {
+  console.log(`Server listening on Port ${PORT}`.yellow.bold);
+});
