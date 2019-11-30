80535e72df65f89b97239b4f564482752cc47529
vic30004 <victor_993@msn.com>
2019-11-30 09:10:20 -0800

    attempting deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/db");
 const colors = require("colors");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const path = require('path')
 const cookieParser = require('cookie-parser')
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -29,7 +30,6 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.get("/", (req, res) => res.send("API Running"));
 // Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
@@ -39,7 +39,16 @@ app.use("/api/forum",require('./routes/api/form'));
 app.use("/api/articles",require("./routes/api/articles"))
 
 
+// Server the build folder
 
+if(process.env.NODE_ENV === 'production'){
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build', 'index.html'))
+  })
+}
 
 // dont put this before routes.
 app.use(errorHandler);
