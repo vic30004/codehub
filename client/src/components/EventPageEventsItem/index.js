84782e689330385b1518a444a2ef1991acc0a11e
shijir0927 <shijiree_mng@yahoo.com>
2019-12-03 21:16:23 -0800

    change idk

--- client/src/components/EventPageEventsItem/index.js
+++ client/src/components/EventPageEventsItem/index.js
@@ -10,11 +10,15 @@ const EventsPageEventsItem = ({ events }) => {
         <h6 class="card-subtitle mb-2 text-muted">
           {event.city_name}, {event.region_abbr}
         </h6>
-        <img
-          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fprivacypolicies.com%2Fblog%2Fwp-content%2Fuploads%2F2015%2F10%2Fcant-miss-tech-events-fi.png&f=1&nofb=1"
-          height="300px"
-          width="400px"
-        />
+        {event.image !== null ? (
+          <img src={event.image.medium.url} height="300px" width="400px" />
+        ) : (
+          <img
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fprivacypolicies.com%2Fblog%2Fwp-content%2Fuploads%2F2015%2F10%2Fcant-miss-tech-events-fi.png&f=1&nofb=1"
+            height="300px"
+            width="400px"
+          />
+        )}
         <p class="card-text">
           <a href={event.url} target="_blank">
             <button>More Info</button>
@@ -26,3 +30,4 @@ const EventsPageEventsItem = ({ events }) => {
 };
 
 export default EventsPageEventsItem;
+s
