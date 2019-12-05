5ab03b0b94083ef624c589e847ce3c117c89794e
Jembae <jgilbertworks@gmail.com>
2019-12-04 22:01:35 -0800

    updated homepage css

--- client/src/components/HomeSlider/index.js
+++ client/src/components/HomeSlider/index.js
@@ -7,6 +7,9 @@ import './style.css';
 import Image1 from '../../assets/images/InnerBanner-webdevelopment.jpg';
 import Image2 from '../../assets/images/web-development-banner.jpg';
 import Image3 from '../../assets/images/Web-Development.jpg';
+// const Image1 = 'https://blog.eduonix.com/wp-content/uploads/2016/11/Full-Stack.png';
+// const Image2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnmmNYkmFMs6dhvpDt0i14COhlj3dFHL3nJt1pV5mJEwMKt8OA&s';
+// const Image3 = 'https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/8fe47e61-afe9-43e9-8b23-41a053701cd9/agibri19.png';
 
 class Hero extends Component {
     constructor(props) {
