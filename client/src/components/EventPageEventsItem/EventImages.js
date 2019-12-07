bad692683208465c33c1c7fa5e126c97bedd0fa9
shijir0927 <shijiree_mng@yahoo.com>
2019-12-07 11:11:56 -0800

    refined some style in events page

---
+++ client/src/components/EventPageEventsItem/EventImages.js
@@ -0,0 +1,23 @@
+const imageURL = [
+  "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fschmoozd.com%2Fwp-content%2Fuploads%2Ftc_disrupt_SF_2017-min.png&f=1&nofb=1",
+
+  "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.professionalconnector.com%2Fwp-content%2Fuploads%2F2018%2F05%2Fhttps_2F2Fcdn.evbuc_.com2Fimages2F449694652F245891737222F12Foriginal.jpg&f=1&nofb=1",
+
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F62b44a55c8f3a87440df-45c7b4d5aac2f388172e587008aaac69.ssl.cf2.rackcdn.com%2Fevent-1274-1530649176-cre-urbantech-summit-sf-large.jpg&f=1&nofb=1",
+
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F3z61v51uhgnmmsubi1n0uv6r-wpengine.netdna-ssl.com%2Fwp-content%2Fuploads%2F2018%2F07%2Fad-tech-sydney.jpg&f=1&nofb=1",
+
+  "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F1.bp.blogspot.com%2F-H0JDxMZbsI4%2FVaphGVrCIbI%2FAAAAAAAAFQY%2FwVFJKJsf5qQ%2Fs1600%2FTECH%25252B%252BInvitation.jpg&f=1&nofb=1",
+
+  "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.scientology.org%2Fsites%2Fdefault%2Fthemes%2Fwww_scientology_org2%2Fimages%2Ftests%2F01-Golden-Age-of-Tech-II-Event-Hall-Alt-RC32667.jpg&f=1&nofb=1",
+
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fnmgprod.s3.amazonaws.com%2Fmedia%2Ffiler_public_thumbnails%2Ffiler_public%2F40%2F9f%2F409f94cb-ceb6-4d6f-9430-9e389850cf06%2Fpro_direct_1.jpg__1920x1920_q85_subsampling-2.jpg&f=1&nofb=1",
+
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.ges.com%2Fimages%2Fdefault-source%2Fdefault-album%2Finteractive-oled.jpg%3Fsfvrsn%3Db981ae80_0&f=1&nofb=1",
+
+  "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.displayworks.com%2Fwp-content%2Fuploads%2F2014%2F02%2Fevent-technology.jpg&f=1&nofb=1",
+
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faquarianllc.com%2Fwp-content%2Fuploads%2F2018%2F01%2FTop-EventTech-Experiential-DesignTrends-for-2018-banner.png&f=1&nofb=1"
+];
+
+export default imageURL;
